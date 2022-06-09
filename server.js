268d9a812e050ac93ea952e1b505ec0be7a025aa
Ardhi <dadinurardhi@gmail.com>
2022-06-09 15:39:40 +0700

    first commit

---
+++ server.js
@@ -0,0 +1,66 @@
+const http = require('http');
+
+const requestListener = (req, res) => {
+  res.setHeader('Content-Type', 'application/json');
+  res.setHeader('X-Powered-By', 'NodeJs');
+  res.statusCode = 200;
+
+  const { method, url } = req;
+
+  if (url === '/') {
+    if (method === 'GET') {
+      res.statusCode = 200;
+      res.end(
+        JSON.stringify({
+          message: 'Ini adalah homepage!',
+        })
+      );
+    } else {
+      res.statusCode = 400;
+      res.end(
+        JSON.stringify({
+          message: `Halaman tidak dapat diakses dengan ${method} request`,
+        })
+      );
+    }
+  } else if (url === '/about') {
+    if (method === 'GET') {
+      res.statusCode = 200;
+      res.end(JSON.stringify({ message: 'Hallo ini adalah halaman about' }));
+    } else if (method === 'POST') {
+      let body = [];
+
+      req.on('data', (chunk) => {
+        body.push(chunk);
+      });
+
+      req.on('end', () => {
+        body = Buffer.concat(body).toString();
+        const { name } = JSON.parse(body);
+        res.statusCode = 200;
+        res.end(
+          JSON.stringify({ message: `Hallo ${name} ini adalah halaman about!` })
+        );
+      });
+    } else {
+      res.statusCode = 400;
+      res.end(
+        JSON.stringify({
+          message: `Halaman tidak dapat diakses menggunakan ${method} request`,
+        })
+      );
+    }
+  } else {
+    res.statusCode = 404;
+    res.end(JSON.stringify({ message: 'Halaman tidak ditemukan!' }));
+  }
+};
+
+const server = http.createServer(requestListener);
+
+const port = 5000;
+const host = 'localhost';
+
+server.listen(port, host, () => {
+  console.log(`Server berjalan pada http://${host}:${port}`);
+});
